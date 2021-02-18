afe7a8d7b2e9287d8928279afbec977d0c1d3b15
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-02-18 15:24:39 +0000

    server and db connected and running, all get and set working through postman

--- server/index.js
+++ server/index.js
@@ -2,13 +2,22 @@ const express = require('express');
 const app = new express();
 const mongoose = require('mongoose');
 const cors = require('cors');
-//TODO: const router = require('./router');
+
+const router = require('./router');
 const PORT = 3001;
 
 app.use(cors());
 app.use(express.json());
-//app.use(router);
+app.use(router);
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}🚀🧨`)
-});
+(async function () {
+  try {
+    await mongoose.connect(`mongodb://localhost:27017/react-topics`, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+    console.log('Connected to db 🗄😍')
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}🚀🧨`)
+    });
+  } catch (err) {
+    console.log('Error', err)
+  }
+}) ();
