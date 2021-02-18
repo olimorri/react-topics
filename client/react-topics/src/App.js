c1bd4d644266fb155b443c8f9bc601d71696eab3
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-02-18 20:34:30 +0000

    feat: all methods working

--- client/react-topics/src/App.js
+++ client/react-topics/src/App.js
@@ -22,16 +22,33 @@ function App() {
     })
   }
 
+  const voteTopic = (id, dir) => {
+    ApiService.voteTopic(id, dir)
+    .then(updatedTopic => {
+      setTopics(topics => {
+        const index = topics.findIndex(topic => topic._id === updatedTopic._id)
+        const tempArr = [...topics]
+        const entryToRemove = tempArr.splice(index, 1, updatedTopic)
+        return tempArr;
+
+      })
+      //we want to remove the item at the index of id and replace it
+      //but we cant do this in place
+      //tempArr is the array after we've taken the old version out
+
+    })
+  }
+
   useEffect(() => {
     ApiService.getTopics()
-    .then(topics => setTopics(topics))
-  }, []);
+    .then(topics => setTopics(topics.sort((a,b) => b.votes - a.votes)))
+  }, [topics]); //we need to put topics in here so that it refreshes on changes to topics
 
 
   return (
     <div className="App">
       <TopicInput createTopic={createTopic}/>
-      <TopicList topics={topics} deleteTopic={deleteTopic}/>
+      <TopicList topics={topics} deleteTopic={deleteTopic} voteTopic={voteTopic}/>
       
     </div>
   );
