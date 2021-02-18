562b9d29fe2e5f66e7c3ce5eb5c754f7841964e1
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-02-18 15:25:07 +0000

    exported router to index.js - get and post working

--- server/router.js
+++ server/router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { getTopics, postTopics } = require('../controllers/topicController');
+const { getTopics, postTopics } = require('./controllers/topicController');
 
 router.get('/topics', getTopics);
 router.post('/topics', postTopics);
 
+module.exports = router; //dont forget this!
