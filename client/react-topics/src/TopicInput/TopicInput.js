7713228edef54eb2856775dd76dee22b225bdf9e
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-02-18 19:13:16 +0000

    feat: created TopicInput component and logic for POST method

---
+++ client/react-topics/src/TopicInput/TopicInput.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import './TopicInput.css'
+
+function TopicInput(props) {
+
+  const [input, setInput] = useState('')
+
+  const handleChange = (event) => {
+    setInput(event.target.value)
+  }
+
+  function handleClick(e) {
+    console.log('clicked')
+    e.preventDefault();
+    props.createTopic(input);
+    setInput('');
+  }
+
+  return (
+    <form className="topic_input_container" onSubmit={handleClick}>
+      <input type="text" className="topic_input_text" value={input} onChange={handleChange}/>
+      <button type="submit" className="topic_input_btn">Create</button>
+    </form>
+  )
+
+}
+
+export default TopicInput
